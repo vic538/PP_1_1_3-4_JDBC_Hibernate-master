f18100cb4c2fd7456f34fe3a6df27526feb8fa24
vic538 <777vvv2015@gmail.com>
2025-07-14 14:12:14 +0300

    Add files via upload

---
+++ PP_1_1_3-4_JDBC_Hibernate-master/PP_1_1_3-4_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,60 @@
+package jm.task.core.jdbc.util;
+
+
+import jm.task.core.jdbc.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+import javax.imageio.spi.ServiceRegistry;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class Util {
+
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/kataacademy";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "";
+    private SessionFactory sessionFactory;
+
+    public Connection getConnection() {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    return conn;
+    }
+
+    public SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+                Properties properties = new Properties();
+                properties.setProperty("hibernate.connection.url", URL);
+                properties.setProperty("hibernate.connection.username", USERNAME);
+                properties.setProperty("hibernate.connection.password", PASSWORD);
+                properties.setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
+                properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+                properties.setProperty("hibernate.show_sql", "true");
+                properties.setProperty("hibernate.format_sql", "true");
+                properties.setProperty("hibernate.hbm2ddl.auto", "update");
+
+                configuration.setProperties(properties);
+                configuration.addAnnotatedClass(User.class);
+                StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    return sessionFactory;
+    }
+}
